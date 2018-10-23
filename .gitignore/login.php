7dbcdf1946ff0cffb83b792e12ec92b9578df297
jedusei <jeduseiubs@gmail.com>
2018-10-23 22:53:49 +0100

    Update and rename .gitignore to login.php

---
+++ .gitignore/login.php
@@ -0,0 +1,23 @@
+<?php
+    $con = mysqli_connect("sql7.freesqldatabase.com", "sql7262438", "cUtBIEPwFm", "sql7262438");
+    
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "SELECT name FROM users WHERE Email = ? AND Password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $email, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $name);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;  
+        $response["name"] = $name;
+    }
+    
+    echo json_encode($response);
+?>
